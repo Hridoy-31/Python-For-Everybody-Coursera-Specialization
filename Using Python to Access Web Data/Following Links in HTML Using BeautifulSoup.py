497aa0e43666f2cc5ead40f122d025540fa5284c
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-08-24 21:49:40 +0600

    Initial commit

---
+++ Using Python to Access Web Data/Following Links in HTML Using BeautifulSoup.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+count  = int(input('Enter count: '))
+pos = int(input('Enter position: '))
+print('Retrieving: ', url)
+
+for i in range(count):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    print('Retrieving: ', tags[pos-1].get('href', None))
+    url = tags[pos-1].get('href', None)
+    
